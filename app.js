760e003405eb8f6a175f37bc524be44d73573b7f
HNafiii <76960651+HNafiii@users.noreply.github.com>
2026-02-21 05:23:41 +0700

    Update app.js
    Add 'components' sub directory on import module name.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { CycleButton } from "./cycle-button.js";
+import { CycleButton } from "./components/cycle-button.js";
 export default function app() {
   return <CycleButton values={["Easy", "Normal", "Hard"]} />;
 }
