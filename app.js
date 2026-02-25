318e7cd185cba29867804b6514585f4a48efb7bd
HNafiii <76960651+HNafiii@users.noreply.github.com>
2026-02-25 21:59:36 +0700

    Update app.js
    Updating components directory
    Adding 'cell.js'

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { CycleButton } from "./components/cycle-button.js";
+import { Cell } from "./components/cell.js";
 export default function app() {
   return <CycleButton values={["Easy", "Normal", "Hard"]} />;
 }
