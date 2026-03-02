098c9c85adeab8b71f9cef4ecbb9ae55f3d542d4
HNafiii <76960651+HNafiii@users.noreply.github.com>
2026-03-02 13:47:53 +0700

    Update cell.js
    Fix some codes. Change `direction` to `relativePosition`.

--- components/cell.js
+++ components/cell.js
@@ -1,27 +1,27 @@
 export default function Cell({ row, column, isMine, revealed, flagged, count, neighbours }) {
   if (!neighbours) return;
   // Labelling Function
-  const filterNeighbours = (key, value) => neighbours.filter((neighbour) => !neighbour.revealed && neighbour[key] === value);
+  const filterNeighbours = (key, value) => neighbours.filter(neighbour => !neighbour.revealed && neighbour[key] === value);
   const getCount = (neighbours) => neighbours.length;
-  const manipulateDirectionsText = (neighbours) => neighbours.map((neighbour) => `at the ${neighbour.direction}`);
-  const generateDirectionsLabel = (count, directions) => (count < neighbours.length ? ` ${count > 2 ? `${[directions.slice(0, -1).join(", "), directions.at(-1)].join(", and ")}` : directions.join(" and ")}` : "");
-  const generateLabel = (count, text, directionsLabel, direction) => (count ? `${count} ${text} neighbour cell${count > 2 ? "s" : ""}${directionsLabel}` : "");
+  const generatePrepositions = (neighbours) => neighbours.map(neighbour => `at the ${neighbour.relativePosition}`);
+  const generateRelativePositionsLabel = (count, relativePositions) => count < neighbours.length ? ` ${count > 2 ? `${[relativePositions.slice(0, -1).join(", "), relativePositions.at(-1)].join(", and ")}` : relativePositions.join(" and ")}` : "";
+  const generateLabel = (count, text, relativePositionsLabel) => count ? `${count} ${text} neighbour cell${count > 2 ? "s" : ""}${relativePositionsLabel}` : "";
   // Neighbours cells labelling
   const concealedNeighbours = filterNeighbours("revealed", false);
   const fullyFlaggedNeighbours = filterNeighbours("flagged", true);
   const halfFlaggedNeighbours = filterNeighbours("flagged", "?");
-  const concealedNeighbourCount = getCount(concealedNeighbours);
-  const fullyFlaggedNeighbourCount = getCount(fullyFlaggedNeighbours);
-  const halfFlaggedNeighbourCount = getCount(halfFlaggedNeighbours);
-  const concealedNeighbourDirections = manipulateDirectionsText(concealedNeighbours);
-  const fullyFlaggedNeighbourDirections = manipulateDirectionsText(fullyFlaggedNeighbours);
-  const halfFlaggedNeighbourDirections = manipulateDirectionsText(halfFlaggedNeighbours);
-  const concealedNeighbourDirectionsLabel = generateDirectionsLabel(concealedNeighbourCount, concealedNeighbourDirections);
-  const fullyFlaggedNeighbourDirectionsLabel = generateDirectionsLabel(fullyFlaggedNeighbourCount, fullyFlaggedNeighbourDirections);
-  const halfFlaggedNeighbourDirectionsLabel = generateDirectionsLabel(halfFlaggedNeighbourCount, halfFlaggedNeighbourDirections);
-  const concealedNeighboursLabel = generateLabel(concealedNeighbourCount, "concealed", concealedNeighbourDirectionsLabel);
-  const fullyFlaggedNeighboursLabel = generateLabel(fullyFlaggedNeighbourCount, "fully flagged", fullyFlaggedNeighbourDirectionsLabel);
-  const halfFlaggedNeighboursLabel = generateLabel(halfFlaggedNeighbourCount, "half-flagged", halfFlaggedNeighbourDirectionsLabel);
+  const concealedNeighbourCount = getCount (concealedNeighbours);
+  const fullyFlaggedNeighbourCount = getCount (fullyFlaggedNeighbours);
+  const halfFlaggedNeighbourCount = getCount (halfFlaggedNeighbours);
+  const concealedNeighbourPrepositions = generatePrepositions(concealedNeighbours);
+  const fullyFlaggedNeighbourPrepositions = generatePrepositions(fullyFlaggedNeighbours);
+  const halfFlaggedNeighbourPrepositions = generatePrepositions(halfFlaggedNeighbours);
+  const concealedNeighbourRelativePositionsLabel = generateRelativePositionsLabel(concealedNeighbourCount, concealedNeighbourPrepositions);
+  const fullyFlaggedNeighbourRelativePositionsLabel = generateRelativePositionsLabel(fullyFlaggedNeighbourCount, fullyFlaggedNeighbourPrepositions);
+  const halfFlaggedNeighbourRelativePositionsLabel = generateRelativePositionsLabel(halfFlaggedNeighbourCount, halfFlaggedNeighbourPrepositions);
+  const concealedNeighboursLabel = generateLabel(concealedNeighbourCount, "concealed", concealedNeighbourRelativePositionsLabel);
+  const fullyFlaggedNeighboursLabel = generateLabel(fullyFlaggedNeighbourCount, "fully flagged", fullyFlaggedNeighbourRelativePositionsLabel);
+  const halfFlaggedNeighboursLabel = generateLabel(halfFlaggedNeighbourCount, "half-flagged", halfFlaggedNeighbourRelativePositionsLabel);
   const neighboursLabels = [concealedNeighboursLabel, fullyFlaggedNeighboursLabel, halfFlaggedNeighboursLabel].filter(Boolean);
   const neighboursLabel = neighboursLabels.length ? ` (${neighboursLabels.join(", ")})` : "";
   // Current cell labelling
@@ -33,7 +33,9 @@ export default function Cell({ row, column, isMine, revealed, flagged, count, ne
   const countText = count ? count : "";
   const revealedText = isMine ? "X" : countText;
   const concealedText = flagged === "?" ? flagged : flagged ? "F" : "";
-  const cellText = revealed ? revealedText : concealedText;
+  const cellText = revealed? revealedText : concealedText;
+  return <button aria-label={`${cellLabel} at row  ${row} column ${column}${neighboursLabel}.`}>{cellText}</button>;
+}
   return <button aria-label={`${cellLabel} at row  ${row} column ${column}${neighboursLabel}.`}>{cellText}</button>;
 }
 }
